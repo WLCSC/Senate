980bcd3fa3ee3a9eaf052d826da3346854656c8c
Brad Thompson <brad@bradsotherbrain.com>
2012-05-28 13:32:05 -0400

    added mail config

--- config/initializers/app_config.rb
+++ config/initializers/app_config.rb
@@ -1,2 +1,10 @@
 raw_config = File.read("#{Rails.root}/config/app_config.yml")
 APP_CONFIG = YAML.load(raw_config)[Rails.env].symbolize_keys
+
+ActionMailer::Base.smtp_settings = {  
+   :address              => APP_CONFIG[:email_server],  
+   :port                 => APP_CONFIG[:email_server_port],  
+   :domain               => APP_CONFIG[:ldap_domain_email],  
+   :enable_starttls_auto => APP_CONFIG[:email_tls_auto]  
+}  
+
